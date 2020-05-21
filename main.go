ceeb3862f0438e87927fc825d4d5d838486ad16d
Muslimah94 <ospanova.m.e@gmail.com>
2020-05-22 01:02:15 +0600

    middleware is implemented

--- main.go
+++ main.go
@@ -96,27 +96,3 @@ func (a *app) NotRequireAuthMiddleware(handleFunc hendler) http.HandlerFunc {
 		handleFunc(a.db, w, r)
 	}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-
-// 	dbase "./dbase"
-// 	mplx "./mplx"
-// )
-
-// func main() {
-// 	db, err := dbase.Create("forumDB")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
-// 		mplx.Multiplexer(db, w, r)
-// 	})
-
-// 	fmt.Println("Server is listening to port :8080...")
-// 	http.ListenAndServe(":8080", nil)
-// }
